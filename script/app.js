91a0025f68e78e9807864561abba907dfefd369a
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-08-30 14:20:48 +0530

    done with little validation

--- script/app.js
+++ script/app.js
@@ -14,7 +14,6 @@ form.addEventListener('submit', (e) => {
         showMessage("Please enter your meal..")
     } else {
         fecthMeal(searchMeal)
-        document.querySelector('.mess').style.display = "none"
     }
 
 })
@@ -22,30 +21,32 @@ form.addEventListener('submit', (e) => {
 function fecthMeal(meal) {
 
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${meal}`)
-        
+
         .then((res) => {
             return res.json()
         })
 
         .then((data) => {
             var mealCard = ''
-            if (data.meals == null) {
+            if (data.meals) {
+                data.meals.forEach((eachMeal) => {
+                    mealCard += `
+                   
+                   <div class="meal-card" data-id="${eachMeal.idMeal}">
+                   <img src="${eachMeal.strMealThumb}">
+                   <div class="meal-content">
+                        <h3>${eachMeal.strMeal}</h3>
+                        <button class="btn-recipe">Get Recipe</button>
+                   </div>
+                   </div>
+        
+                   `
+                    document.querySelector('.meal-row').innerHTML = mealCard;
+                    document.querySelector('.mess').style.display = 'none';
+                })
+            } else if (data.meals == null) {
                 showMessage("Sorry! we didn't find any meal.")
             }
-            data.meals.forEach((eachMeal) => {
-            mealCard += `
-           
-           <div class="meal-card" data-id="${eachMeal.idMeal}">
-           <img src="${eachMeal.strMealThumb}">
-           <div class="meal-content">
-                <h3>${eachMeal.strMeal}</h3>
-                <button class="btn-recipe">Get Recipe</button>
-           </div>
-           </div>
-
-           `
-                document.querySelector('.meal-row').innerHTML = mealCard;
-            })
         })
         .catch((error) => {
             showMessage(`Some went worng: ${error}`)
@@ -55,5 +56,48 @@ function fecthMeal(meal) {
 
 
 function showMessage(str) {
+    // console.log(str)
     document.querySelector('.mess').innerHTML = str
+}
+
+
+document.querySelector('.meal-row').addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (e.target.classList.contains('btn-recipe')) {
+        var mealCard = e.target.parentElement.parentElement;
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealCard.dataset.id}`)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                var details = ''
+                data.meals.forEach((eachData) => {
+                    console.log(eachData)
+                    if (eachData) {
+                        details += `
+                            <div class="recipe-details-container">
+                            <h1>${eachData.strMeal}</h1>
+                            <h5>${eachData.strCategory}</h5>
+                            <p>${eachData.strInstructions}</p>
+                            <img src="${eachData.strMealThumb}">
+                            <a href="${eachData.strYoutube}" target="_blank"><button>Watch Video</button></a>
+                            <i class="fas fa-times"  id="btn-cut" onclick="removeDetails()"></i>
+                            </div>
+                   
+                   `
+                   document.querySelector('.meal-row').style.display = 'none'
+                   document.querySelector('.recipe-details').style.display = 'flex'
+                   document.querySelector('.recipe-details').innerHTML = details;
+                    }
+                })
+            })
+    }
+})
+
+
+function removeDetails(){
+    document.querySelector('.meal-row').style.display = 'flex'
+    document.querySelector('.recipe-details').style.display = 'none'
 }
